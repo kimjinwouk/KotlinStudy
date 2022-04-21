9cc1921e558b88d0a04ec3054c4d23c99a66b98c
Jin Kim <crous1@naver.com>
2022-04-21 16:35:47 +0900

    Lotto

---
+++ Lotto/app/src/main/java/com/kimjinwouk/lotto/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.kimjinwouk.lotto
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.zxing.integration.android.IntentIntegrator
+import com.kimjinwouk.lotto.Fragment.homeFragment
+import com.kimjinwouk.lotto.Fragment.placeFragment
+
+
+class SplashActivity : BaseActivity() {
+
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+    }
+
+
+    private fun init() {
+
+    }
+
+    private fun initListner() {
+
+
+
+    }
+
+}
