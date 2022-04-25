8014b6486375bd70be74f958eb180cc017fabf71
Jin Kim <crous1@naver.com>
2022-04-25 17:48:59 +0900

    Lotto

--- Lotto/app/src/main/java/com/kimjinwouk/lotto/Fragment/placeFragment.kt
+++ Lotto/app/src/main/java/com/kimjinwouk/lotto/Fragment/placeFragment.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.kimjinwouk.lotto.Kakao.KakaoApi
 import com.kimjinwouk.lotto.Kakao.KakaoData
 import com.kimjinwouk.lotto.MainActivity
@@ -29,6 +32,7 @@ class placeFragment : Fragment() {
     lateinit var _MainActivity: MainActivity
     lateinit var mapView: MapView
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,8 +42,50 @@ class placeFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_place, container, false)
         mapView = MapView(activity)
         val mapViewContainer: ViewGroup = view.findViewById(R.id.map_view)
+
         mapViewContainer.addView(mapView)
         _MainActivity = activity as MainActivity
+
+        bottomSheetBehavior = BottomSheetBehavior.from(view.findViewById(R.id.bottomSheet))
+
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                // handle onSlide
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> Toast.makeText(
+                        context,
+                        "STATE_COLLAPSED",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    BottomSheetBehavior.STATE_EXPANDED -> Toast.makeText(
+                        context,
+                        "STATE_EXPANDED",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    BottomSheetBehavior.STATE_DRAGGING -> Toast.makeText(
+                        context,
+                        "STATE_DRAGGING",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    BottomSheetBehavior.STATE_SETTLING -> Toast.makeText(
+                        context,
+                        "STATE_SETTLING",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    BottomSheetBehavior.STATE_HIDDEN -> Toast.makeText(
+                        context,
+                        "STATE_HIDDEN",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    else -> Toast.makeText(context, "OTHER_STATE", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
 
         return view
     }
