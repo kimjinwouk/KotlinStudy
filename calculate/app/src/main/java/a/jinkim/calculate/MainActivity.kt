9f9f34ebade84f7bda86108d570d6c2d1304e06f
Jin Kim <crous1@naver.com>
2022-05-31 11:20:27 +0900

    Calculate

---
+++ calculate/app/src/main/java/a/jinkim/calculate/MainActivity.kt
@@ -0,0 +1,11 @@
+package a.jinkim.calculate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
